46f8521f05669b6931b3dd77ce958c1beb13b7cf
Gabriela Cardoso <157655310+gabicards@users.noreply.github.com>
2024-05-17 14:22:12 +0200

    Pt 1

--- db/seeds.rb
+++ db/seeds.rb
@@ -1,9 +1,15 @@
-# This file should ensure the existence of records required to run the application in every environment (production,
-# development, test). The code here should be idempotent so that it can be executed at any point in every environment.
-# The data can then be loaded with the bin/rails db:seed command (or created alongside the database with db:setup).
-#
-# Example:
-#
-#   ["Action", "Comedy", "Drama", "Horror"].each do |genre_name|
-#     MovieGenre.find_or_create_by!(name: genre_name)
-#   end
+puts "Cleaning database..."
+Restaurant.destroy_all
+
+puts "Creating restaurants..."
+dishoom = {name: "Dishoom", address: "7 Boundary St, London E2 7JE", phone_number: "01632 960237", category: "italian"}
+pizza_east =  {name: "Pizza East", address: "56A Shoreditch High St, London E1 6PQ", phone_number: "01632 960834", category: "italian"}
+belgian_paradise = {name: "Belgian Paradise", address: "58B Easy St, London E6 9PQ", phone_number: "01632 960243", category: "belgian"}
+pizza_lore = {name: "Pizza Lore", address: "5A Cornelia St, London E1 6PQ", phone_number: "01632 960216", category: "italian"}
+marrakech_service = {name: "Marrakech Service", address: "23A Nimbus High St, London 3PQ", phone_number: "01632 960248", category: "french"}
+
+[dishoom, pizza_east, belgian_paradise, pizza_lore, marrakech_service].each do |attributes|
+  restaurant = Restaurant.create!(attributes)
+  puts "Created #{restaurant.name}"
+end
+puts "Finished!"
